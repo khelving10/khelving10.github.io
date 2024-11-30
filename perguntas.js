684e728ea2dac8e49c2d5a1a38177b12fb442aea
khelving10 <khelvingabriel2007@gmail.com>
2024-11-30 15:23:34 -0300

    upload arquivos site flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Saúde',
+    'Por que é importante dormir bem?',
+    'Dormir bem é essencial para a saúde mental e física, melhorando a memória, a concentração e fortalecendo o sistema imunológico.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'Por que a segurança digital é importante?',
+    'A segurança digital protege dados e sistemas contra ataques cibernéticos, garantindo privacidade e segurança online.'
+)
+
+criaCartao(
+    'Direitos e Deveres',
+    'Quais são os direitos e deveres de um cidadão?',
+    'Os direitos de um cidadão incluem liberdade de expressão, direito à educação e saúde, enquanto os deveres envolvem cumprir as leis, votar e respeitar os direitos dos outros.'
+)
+
+criaCartao(
+    'Ética',
+    'Por que é importante ter ética?',
+    'Ter ética é importante porque ela guia nossas ações, promove o respeito mútuo, constrói confiança e mantém a justiça e o equilíbrio nas relações pessoais e profissionais.'
+)
